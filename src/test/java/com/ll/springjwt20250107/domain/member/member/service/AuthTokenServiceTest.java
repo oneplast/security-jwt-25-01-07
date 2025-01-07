1d8e171b1be369b2875b771970316a32d375b6d8
Gyeonghun Min <riverpower6@naver.com>
2025-01-07 12:33:13 +0900

    refactor: Ut - get JWT token

--- src/test/java/com/ll/springjwt20250107/domain/member/member/service/AuthTokenServiceTest.java
+++ src/test/java/com/ll/springjwt20250107/domain/member/member/service/AuthTokenServiceTest.java
@@ -2,12 +2,14 @@ package com.ll.springjwt20250107.domain.member.member.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.ll.springjwt20250107.util.Ut;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.Date;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,15 @@ class AuthTokenServiceTest {
     @Autowired
     private AuthTokenService authTokenService;
 
+    @Autowired
+    private MemberService memberService;
+
+    // 토큰 만료기간 : 1년
+    private int expireSeconds = 60 * 60 * 24 * 365;
+
+    // 토큰 시크릿 키
+    private String secret = "askdfhsdalkghsdaklghsdaklghsadilghewioo12i4o21498ht98ag0m1t099omaopfm-012k4epo";
+
     @Test
     @DisplayName("authTokenService 서비스가 존재한다.")
     void t1() {
@@ -31,12 +42,6 @@ class AuthTokenServiceTest {
     @Test
     @DisplayName("jjwt로 JWT 생성, {name=\"Paul\", age=23}")
     void t2() {
-        // 토큰 만료기간 : 1년
-        int expireSeconds = 60 * 60 * 24 * 365;
-        // 토큰 시크릿 키
-        Key secretKey = Keys.hmacShaKeyFor(
-                "askdfhsdalkghsdaklghsdaklghsadilghewioo12i4o21498ht98ag0m1t099omaopfm-012k4epo".getBytes());
-
         Claims claims = Jwts.claims()
                 .add("name", "Paul")
                 .add("age", 23)
@@ -45,12 +50,24 @@ class AuthTokenServiceTest {
         Date issuedAt = new Date();
         Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
 
+        Key secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+
         String jwt = Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(issuedAt)
                 .setExpiration(expiration)
                 .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
+
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt = " + jwt);
+    }
+
+    @Test
+    @DisplayName("Ut.jwt.toString 을 통해서 JWT 생성, {name=\"Paul\", age=23}")
+    void t3() {
+        String jwt = Ut.jwt.toString(secret, expireSeconds, Map.of("name", "Paul", "age", 23));
 
         assertThat(jwt).isNotBlank();
 
