7cb9408e229602f17aa453606589c83a0de2f9f0
Gyeonghun Min <riverpower6@naver.com>
2025-01-07 10:55:34 +0900

    settings

---
+++ src/main/java/com/ll/springjwt20250107/SpringJwt20250107Application.java
@@ -0,0 +1,13 @@
+package com.ll.springjwt20250107;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwt20250107Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwt20250107Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/springjwt20250107/SpringJwt20250107ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.springjwt20250107;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwt20250107ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
